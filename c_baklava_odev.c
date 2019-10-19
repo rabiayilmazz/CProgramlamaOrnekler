f0653b760b7df6920f1e3b2b0143237557dc8c3a
rabia yılmaz <45205525+rbylmz72242@users.noreply.github.com>
2019-10-19 23:13:52 +0300

    Add files via upload
    c

---
+++ c_baklava_odev.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int i, j, y, b, s;
+	
+	printf("Sayi giriniz: ");
+	
+	scanf("%d", &s);
+	
+	b=s; 
+	y=1;
+	
+	for(i = 0 ; i < s ; i++)
+	{
+		for(j = 1 ; j <= b ; j++)
+		{
+			printf(" ");
+		}
+		
+		b--;
+		
+		for(j = 1 ; j <= y ; j++)
+		{
+			printf("*");
+		}
+		
+		y = y + 2;			
+		
+		printf("\n");
+	} 
+	  
+		y = y - 4;
+		
+	for(i = 1 ; i < s ; i++)
+	{
+		for(j = 0 ; j <= i ; j++)
+		{
+			printf(" ");
+		}
+		
+		b++;
+		
+		for(j = 1 ; j <= y ; j++)
+		{
+			printf("*");
+		}
+		
+		
+		y = y - 2;			
+		
+		printf("\n");
+	} 
+	return 0;	
+}
