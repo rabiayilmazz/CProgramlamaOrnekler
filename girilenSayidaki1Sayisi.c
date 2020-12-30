7c4e1c043c6abe474adfa3628232cd6ae1abb2d2
rabia yılmaz <rabiarabiabbb@gmail.com>
2020-12-30 17:35:50 +0300

    Sayıdaki bir rakamın tekrarını bulma

---
+++ girilenSayidaki1Sayisi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x,y,z;
+	printf("Please enter a value:");
+	scanf("%d",&x);
+	while(x!=0){
+		y=x%10;
+		if(y==1) {
+		z++;
+		}
+		x=x/10;
+	}
+	printf("%i",z);
+}
