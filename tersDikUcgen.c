c3ccca84d5a4fca4fbdc11dd06e8f1fae06a43d6
rabia yılmaz <rabiarabiabbb@gmail.com>
2020-12-30 17:34:23 +0300

    dik üçgen

---
+++ tersDikUcgen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a,b;
+for (a=7;a>0;a--)
+{
+    for (b=7;b>0;b--)
+    {
+        if(a<b){
+		}
+		else printf("*");
+    }
+    printf("\n");
+}
+
+}
